40fc9f63fd59eac08eb699a46ed3f29b0e5b1abe
Jenkins for ownCloud <owncloud-bot@tmit.eu>
2016-11-10 02:39:22 +0100

    [tx-robot] updated from transifex

---
+++ l10n/he.js
@@ -0,0 +1,6 @@
+OC.L10N.register(
+    "market",
+    {
+    "Market" : "חנות"
+},
+"nplurals=2; plural=(n != 1);");
---
+++ l10n/pt_BR.js
@@ -0,0 +1,6 @@
+OC.L10N.register(
+    "market",
+    {
+    "Market" : "Mercado"
+},
+"nplurals=2; plural=(n > 1);");
