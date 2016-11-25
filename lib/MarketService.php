56693d80f38f46a1145b56c99764e6bf6f9d5e19
Thomas Müller <thomas.mueller@tmit.eu>
2016-11-25 17:02:45 +0100

    Integrate new marketplace

--- lib/MarketService.php
+++ lib/MarketService.php
@@ -22,130 +22,41 @@
 
 namespace OCA\Market;
 
-use OCA\Market\AppStore\OCSClient;
+use OCA\Market\Marketplace\Service;
 use OCP\App\IAppManager;
 use OCP\IAppConfig;
+use OCP\ICacheFactory;
+use OCP\IConfig;
 
 class MarketService {
 
-	public function __construct(IAppManager $appManager, IAppConfig $appConfig) {
-		$this->appManager = $appManager;
-		$this->appConfig = $appConfig;
+	public function __construct(IConfig $config, IAppManager $appManager, IAppConfig $appConfig, ICacheFactory $cacheFactory) {
+		$storeUrl = $config->getSystemValue('appstoreurl', 'https://api.owncloud.com/v1');
+
+		if ($storeUrl === 'https://api.owncloud.com/v1') {
+			$this->impl = new AppStore\Service($appManager, $appConfig);
+		} else {
+			$this->impl = new Service($appManager, $appConfig, $cacheFactory, $storeUrl);
+		}
 	}
 
 	public function installApp($ocsId) {
-
-		$info = $this->getInstalledAppInfo($ocsId);
-		if (!is_null($info)) {
-			throw new \InvalidArgumentException('App is already installed');
-		}
-
-		// download package
-		$package = $this->downloadPackage($ocsId);
-		$this->appManager->installApp($package);
-	}
-
-	private function downloadPackage($ocsId) {
-		// implementation based on the current appstore - needs factory
-		$ocsClient = new OCSClient(
-			\OC::$server->getHTTPClientService(),
-			\OC::$server->getConfig(),
-			\OC::$server->getLogger()
-		);
-		$download = $ocsClient->getApplicationDownload($ocsId, \OCP\Util::getVersion());
-		if(isset($download['downloadlink']) and $download['downloadlink'] !== '') {
-			// Replace spaces in download link without encoding entire URL
-			$downloadLink = str_replace(' ', '%20', $download['downloadlink']);
-
-			$pathInfo = pathinfo($downloadLink);
-			$extension = isset($pathInfo['extension']) ? '.' . $pathInfo['extension'] : '';
-			$path = \OC::$server->getTempManager()->getTemporaryFile($extension);
-			$client = \OC::$server->getHTTPClientService()->newClient();
-			$client->get($downloadLink, ['save_to' => $path]);
-
-			return $path;
-		}
-
-		throw new \Exception('Unknown app');
+		$this->impl->installApp($ocsId);
 	}
 
 	public function isAppInstalled($ocsId) {
-		$info = $this->getInstalledAppInfo($ocsId);
-		return !is_null($info);
+		return $this->impl->isAppInstalled($ocsId);
 	}
 
 	public function updateAvailable($ocsId) {
-		$info = $this->getInstalledAppInfo($ocsId);
-		if (is_null($info)) {
-			throw new \InvalidArgumentException("App ($ocsId) is not installed");
-		}
-		$marketInfo = $this->getAppInfo($ocsId);
-		if (is_null($marketInfo)) {
-			throw new \InvalidArgumentException("App ($ocsId) is not known at the marketplace.");
-		}
-		$marketVersion = (string) $marketInfo['version'];
-		$currentVersion = (string) $info['version'];
-		if (version_compare($marketVersion, $currentVersion, '>')) {
-			return $marketVersion;
-		}
-		return false;
-	}
-
-	private function getAppInfo($ocsId) {
-		//
-		// TODO: read from cached information as stored by using ./occ market:update
-		//
-		$ocsClient = new OCSClient(
-			\OC::$server->getHTTPClientService(),
-			\OC::$server->getConfig(),
-			\OC::$server->getLogger()
-		);
-		return $ocsClient->getApplication($ocsId, \OCP\Util::getVersion());
-	}
-
-	private function getInstalledAppInfo($ocsId) {
-		$apps = $this->appManager->getAllApps();
-		foreach ($apps as $app) {
-			$info = $this->appManager->getAppInfo($app);
-			if (isset($info['ocsid']) && (int)$info['ocsid'] === (int)$ocsId) {
-				return $info;
-			}
-		}
-
-		return null;
+		return $this->impl->getAvailableUpdateVersion($ocsId);
 	}
 
 	public function updateApp($ocsId) {
-		$info = $this->getInstalledAppInfo($ocsId);
-		if (is_null($info)) {
-			throw new \InvalidArgumentException('App is not installed');
-		}
-
-		// download package
-		$package = $this->downloadPackage($ocsId);
-		$this->appManager->updateApp($package);
+		$this->impl->updateApp($ocsId);
 	}
 
 	public function getUpdates() {
-		$result = [];
-		$apps = $this->appManager->getAllApps();
-		foreach ($apps as $app) {
-			$info = $this->appManager->getAppInfo($app);
-			if (isset($info['ocsid'])) {
-				try {
-					$ocsId = $info['ocsid'];
-					$newVersion = $this->updateAvailable($ocsId);
-					if ($newVersion) {
-						$result[$app] = [
-							'version' => $newVersion,
-							'ocsid' => $ocsId
-						];
-					}
-				} catch (\InvalidArgumentException $ex) {
-				}
-			}
-		}
-
-		return $result;
+		return $this->impl->getUpdates();
 	}
 }
