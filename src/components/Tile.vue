122c9ec2b3ee4be1823b2e3cc1cbf5be64aac4e3
Felix Heidecke <felix@heidecke.me>
2017-04-24 15:03:40 +0200

    Improve UI (WIP)

--- src/components/Tile.vue
+++ src/components/Tile.vue
@@ -1,25 +1,31 @@
 <template lang="pug">
 	li(class='uk-width-1-2@m uk-width-1-3@xl', v-if="application")
 		.uk-card.uk-card-default
-			.uk-card-media-top
-				img(:src="application.screenshots[0].url", :alt="application.name")
-
 			.uk-card-header
-				h3.uk-card-title.uk-text-truncate.uk-margin-remove-bottom
-					router-link(:to="{ name: 'details', params: { id: application.id }}") {{ application.name }}
+				div(uk-grid)
+					.uk-width-expand
+						.uk-flex.uk-flex-middle
+							h3.uk-card-title.uk-text-truncate.uk-margin-remove-bottom.uk-float-left.uk-margin-small-right
+								router-link(:to="{ name: 'details', params: { id: application.id }}") {{ application.name }}
 
-				p.uk-text-meta.uk-margin-remove-top
-					span(uk-icon="icon: tag").uk-margin-small-right
-					| {{ application.categories[0] }}
+						p.uk-text-meta.uk-margin-remove-top
+							span(uk-icon="icon: tag").uk-margin-small-right
+							span.category {{ application.categories[0] }}
+
+					.uk-width-small.uk-text-right
+						rating(:rating="application.rating")
+
+			.uk-card-media-top
+				img(:src="application.screenshots[0].url", :alt="application.title")
 
 			.uk-card-body
+				//p.uk-text-bold
+					span.uk-margin-small-right By:
+					a(:href="application.publisher.url", target="_blank") {{ application.publisher.name }}
+
 				p {{ application.description }}
 
-				div(uk-grid, class="uk-child-width-1-2@s")
-					div
-						rating(:rating="application.rating")
-					div
-						router-link(:to="{ name: 'details', params: { id: application.id }}").uk-button.uk-button-default.uk-button-small.uk-align-right {{ t.more }}
+				router-link(:to="{ name: 'details', params: { id: application.id }}").uk-button.uk-button-default.uk-button-small.uk-align-right {{ t.more }}
 </template>
 
 <script>
@@ -57,5 +63,9 @@
 
 	.fade-enter, .fade-leave-to {
 		opacity: 0
+	}
+
+	.category {
+		text-transform: capitalize;
 	}
 </style>
