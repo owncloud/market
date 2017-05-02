51c38e35c3f9eaec59bf84edc53ce589411a3ec4
Felix Heidecke <felix@heidecke.me>
2017-05-02 18:16:30 +0200

    Show App details during loading, improve update view

--- src/components/UpdateList.vue
+++ src/components/UpdateList.vue
@@ -1,13 +1,12 @@
 <template lang="pug">
 	div
-		.uk-position-fixed.uk-position-center(v-show="loading", uk-spinner, uk-icon="icon: spinner")
 		transition(name="fade")
-			.uk-card.uk-card-default(v-if="!loading && !failed && applications")
+			.uk-card.uk-card-default(v-if="!failed && applications")
 				.uk-card-header
 					h2.uk-h3 {{ t('Updates') }}
 
 				.uk-card-body
-					table.uk-table.uk-table-striped
+					table.uk-table.uk-table-hover.uk-table-divider.uk-table-middle
 						thead
 							tr
 								th {{ t('App') }}
@@ -16,22 +15,27 @@
 								th &nbsp;
 						tbody
 							tr(v-for="application in applications")
-								td.uk-text-bold {{ application.name }}
-								td {{ application.publisher.name }}
+								td
+									router-link(:to="{ name: 'details', params: { id: application.id }}").uk-h5 {{ application.name }}
+								td
+									a(:href="application.publisher.url", target="_blank") {{ application.publisher.name }}
 								td
 									span {{ application.installInfo.version }}
 									span(uk-icon="icon: arrow-right").uk-margin-small-left.uk-margin-small-right
 									span {{ application.updateInfo }}
 								td
-									button.uk-button.uk-button-small.uk-button-primary.uk-align-right.uk-margin-remove.uk-position-relative(@click="update(application.id)", :disabled="updating(application.id)")
+									button.uk-button.uk-button-primary.uk-align-right.uk-margin-remove.uk-position-relative(@click="update(application.id)", :disabled="updating(application.id)")
 										span(v-if="updating(application.id)")
-											.uk-position-center-left(uk-spinner, uk-icon="icon: spinner; ratio: 0.4", style="left:5px;")
-											| &nbsp;&nbsp;&nbsp;&nbsp;updating
+											span.uk-position-small.uk-position-center-left(uk-spinner, uk-icon="icon: spinner; ratio: 0.8")
+											span.uk-margin-small-left &nbsp;&nbsp; {{ t('updating') }}
 										span(v-else)
-											| update
-							tr(v-if="applications.length === 0")
+											| {{ t('update') }}
+							tr(v-if="applications.length === 0 && !loading")
 								td(colspan="4").uk-text-center
 									span.uk-text-primary {{ t('All apps are up to date') }}
+							tr(v-show="loading")
+								td(colspan="4").uk-text-center
+									span(uk-spinner, uk-icon="icon: spinner")
 
 </template>
 
@@ -76,13 +80,4 @@
 
 <style lang="scss" scoped>
 	@import "../styles/variables-theme";
-
-	aside {
-		width: 260px;
-	}
-
-	.uk-button-small {
-		line-height: 20px;
-		font-size: 0.775rem;
-	}
 </style>
