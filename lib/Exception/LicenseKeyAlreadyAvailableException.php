3121acec67db0abf39e9a55b03a2556acbd7793f
Philipp Schaffrath <github@philippschaffrath.de>
2017-07-05 15:33:40 +0200

    added exceptions

---
+++ lib/Exception/LicenseKeyAlreadyAvailableException.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace OCA\Market\Exception;
+
+/**
+ * exception that is thrown when a license key is already set.
+ * @package OCA\Market\Exception
+ */
+class LicenseKeyAlreadyAvailableException extends MarketException {
+
+}
---
+++ lib/Exception/MarketException.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace OCA\Market\Exception;
+
+/**
+ * base exception for market app
+ * @package OCA\Market\Exception
+ */
+class MarketException extends \Exception {
+
+}
