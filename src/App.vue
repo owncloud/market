1ca963faf1e25da9aa49f942713ecc1a5120985e
Felix Heidecke <felix@heidecke.me>
2017-11-02 14:00:50 +0100

    Add initial notice

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,28 @@
 
 			main.uk-width-expand
 				router-view
+
+		#scaleout-notice-modal(uk-modal="bg-close : false")
+			.uk-modal-dialog.uk-margin-auto-vertical
+				button.uk-modal-close-default(type='button', uk-close)
+				.uk-modal-header
+						h2.uk-modal-title Important notice
+				.uk-modal-body
+					.uk-alert-danger(uk-alert)
+						p.uk-h5.uk-text-danger Concerning cluster-setup!
+					p We highly recommend <strong>disabling the market app</strong> if this ownCloud instance is hosted <strong>in a clustered environment</strong>. Installing and updating applications via the market app may lead to unforeseen consequences.
+					p If you would like to know more, please visit&nbsp;
+						a.uk-text-primary(href="#") this Website
+						| .
+
+				.uk-modal-footer.uk-flex.uk-flex-middle
+					.uk-width-1-2.uk-text-left
+						label
+							input.uk-checkbox(type="checkbox", v-model="showOnStartup").uk-margin-small-right
+							| Show message on startup
+					.uk-width-1-2.uk-text-right
+						button.uk-button.uk-button-default.uk-modal-close(type='button') Close
+
 </template>
 
 <script>
@@ -14,8 +36,16 @@
 	import Trial from './components/Trial.vue'
 
 	export default {
+		data () {
+			return {
+				"showOnStartup": true
+			}
+		},
 		mounted () {
 			this.$store.dispatch('FETCH_APPLICATIONS');
+			setTimeout( () => {
+				UIkit.modal('#scaleout-notice-modal').toggle();
+			} , 5000);
 		},
 		components: {
 			Navigation,
