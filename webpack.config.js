0a2f1d629ef1425fae38eaef3f59611fdc04f8ca
Thomas Müller <1005065+DeepDiver1975@users.noreply.github.com>
2023-10-24 17:56:56 +0200

    fix: use webpack 5

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const VueLoaderPlugin = require('vue-loader/lib/plugin');
 
 module.exports = {
-	devtool: 'cheap-eval-source-map',
+	mode: 'production',
 	entry: './src/default.js',
 	output : {
 		path: require('path').resolve(__dirname, 'js'),
@@ -11,26 +11,23 @@ module.exports = {
 	module: {
 		rules: [{
 			test: require.resolve('uikit'),
-			loader: 'expose-loader?UIkit'
+			loader: 'expose-loader',
+			options: {
+				exposes: ["UIkit"],
+			},
 		}, {
 			test: /\.js?$/,
 			exclude: /node_modules/,
-			loader: 'babel-loader',
+			use: 'babel-loader',
 		}, {
 			test: /\.scss?$/,
-			loader: 'style-loader!css-loader!sass-loader'
+			use: ['style-loader', 'css-loader', 'sass-loader']
 		}, {
 			test: /\.pug$/,
-			loader: 'vue-pug-loader'
+			use: 'vue-pug-loader'
 		}, {
 			test: /\.vue$/,
 			loader: 'vue-loader',
-			options: {
-				loaders: {
-					scss: 'vue-style-loader!css-loader!sass-loader',
-					less: 'vue-style-loader!css-loader!less-loader'
-				}
-			}
 		}]
 	},
 	plugins: [
